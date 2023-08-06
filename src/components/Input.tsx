1661a8fca102cc826ca9a6380dfd11f588b4a40d
tonisco <tonyumeozulu@gmail.com>
2023-08-06 17:11:07 +0100

    input form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,25 +2,36 @@ import React from "react"
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   labelText: string
+  errorText?: string
+  message?: string
 }
 
-const Input = ({ labelText, ...props }: Props) => {
+const Input = ({ labelText, errorText, message, ...props }: Props) => {
   return (
-    <label
-      htmlFor={props.id}
-      className="relative block w-full rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-    >
-      <input
-        type="text"
-        {...props}
-        className="peer border-none w-full bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
-        placeholder="Username"
-      />
+    <div>
+      <label
+        htmlFor={props.id}
+        className="relative block w-full rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+      >
+        <input
+          type="text"
+          {...props}
+          className="peer border-none w-full bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
+          placeholder={labelText}
+        />
 
-      <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-2/3 bg-white py-0.5 px-1 text-sm text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-focus:top-0 peer-focus:text-sm peer-focus:text-blue-600">
-        {labelText}
-      </span>
-    </label>
+        <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-2/3 bg-white py-0.5 px-1 text-sm text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-focus:top-0 peer-focus:text-sm peer-focus:text-blue-600">
+          {labelText}
+        </span>
+      </label>
+      {message && (
+        <p className="text-xs text-gray-500 font-semibold">
+          <span className="text-blue-500 font-bold">* </span>
+          {message}
+        </p>
+      )}
+      {errorText && <p className="text-xs text-red-500">{errorText}</p>}
+    </div>
   )
 }
 
--- src/pages/Apply/index.tsx
+++ src/pages/Apply/index.tsx
@@ -1,13 +1,53 @@
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { ClipLoader } from "react-spinners"
+import * as yup from "yup"
+
+import { yupResolver } from "@hookform/resolvers/yup"
+
 import Breadcrumb from "../../components/Breadcrumb"
 import Input from "../../components/Input"
 import Layout from "../../components/Layout"
 
+const schema = yup
+  .object({
+    full_name: yup
+      .string()
+      .matches(
+        /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})/,
+        "Full name is not filled correctly",
+      )
+      // .matches(/^[a-zA-Z]+ [a-zA-Z]+$/, "Full name is required")
+      .required(),
+    loan_amount: yup.number().required(),
+    repayment_duration: yup.number().required(),
+    email: yup.string().email(),
+  })
+  .required()
+
+type FormData = yup.InferType<typeof schema>
+
 const Apply = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver<FormData>(schema),
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const onSubmit = (data: FormData) => console.log(data)
+
   return (
     <Layout>
       <Breadcrumb currPage="Apply" heading="Apply for a loan" />
       <section className="px-6 py-14">
-        <form className="max-w-6xl mx-auto flex flex-col gap-8 bg-white px-10 py-10 rounded-lg shadow-lg">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="max-w-6xl mx-auto flex flex-col gap-8 bg-white px-10 py-10 rounded-lg shadow-lg"
+        >
           <div className="mb-6 text-center">
             <h1 className="text-3xl font-semibold mb-2">Loan Application</h1>
             <p className="text-sm text-gray-500">
@@ -15,26 +55,51 @@ const Apply = () => {
               form and get approved within five working hours
             </p>
           </div>
-          <Input labelText="Full name" name="full_name" id="full_name" />
+          <Input
+            labelText="Full name"
+            {...register("full_name")}
+            // id="full_name"
+            errorText={errors.full_name?.message}
+          />
+
           <Input
             labelText="Loan Amount"
-            name="loan_amount"
+            {...register("loan_amount")}
             id="loan_amount"
             type="number"
+            errorText={errors.loan_amount?.message}
           />
+
           <Input
             labelText="Repayment Duration (in months)"
-            name="repayment_duration"
+            {...register("repayment_duration")}
             id="repayment_duration"
             type="number"
+            errorText={errors.repayment_duration?.message}
           />
-          <Input labelText="Email" name="email" id="email" type="email" />
+
+          <Input
+            {...register("email")}
+            labelText="Email"
+            id="email"
+            type="email"
+            errorText={errors.email?.message}
+            message="Email is used to receive the repayment schedule and its optional"
+          />
 
           <button
             type="submit"
-            className="px-6 hover:bg-blue-500 transition-all duration-500 py-2 self-center text-base text-white bg-blue-600 rounded-lg sm:text-lg"
+            disabled={isSubmitting}
+            className="px-6 hover:bg-blue-500 flex items-center gap-2 disabled:cursor-not-allowed disabled:bg-blue-900 transition-all duration-500 py-2 self-center text-base text-white bg-blue-600 rounded-lg sm:text-lg"
           >
-            Apply now
+            {!isSubmitting ? (
+              <p>Apply now</p>
+            ) : (
+              <>
+                <p>Applying</p>
+                <ClipLoader size={16} color="white" />
+              </>
+            )}
           </button>
         </form>
       </section>
